0c7571bad9ee9332a90a60d3210ddfe795cd9bdc
Kareem Adel <adel30320@gmail.com>
2023-09-08 23:36:17 +0300

    Add Cover Preview and use Select2

--- Game_Zone/wwwroot/js/site.js
+++ Game_Zone/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('select').select2();
+});
 
-// Write your JavaScript code.
